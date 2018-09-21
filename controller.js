718d9e28f060971eecf6a4973d0378f454aba828
L245854 <l245854@pn1706924.lanl.gov>
2018-09-21 11:35:25 -0600

    sms functional. Need individual question logic

--- controller.js
+++ controller.js
@@ -1,31 +1,40 @@
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const surveyQs = require('./survey_questions')
-const twiml = new MessagingResponse();
-
 
 module.exports = {
     emergency: (request, res) => {
+
+        // respond with message TwiML content
+
+        function respond(message) {
+            var twiml = new MessagingResponse();
+            twiml.message(message);
+            res.type('text/xml');
+            res.send(twiml.toString());
+        }
+
+        // console.log(request)
         let { Body, From } = request.body
         let db = request.app.get('db')
-        if (Body.toLowerCase().match(/^(emergency)$/)) {
-            console.log('We have received your request for assistance. We contacted you health worker to coordinate a response.')
-            db.alertStatus(From).then(response => {
-                console.log(response)
-                // response.status(200).send()
+        if (Body.toLowerCase().match(/^.*(emergency).*$/)) {
+            db.alertStatus(From).then(emergencyResponse => {
+                // console.log(response)
+                return respond('We have received your request for assistance. We contacted your health worker to coordinate a response.')
+                res.status(200).send(emergencyResponse)
             })
-            console.log('Your patient {name} at location {Insert location} needs assistance')
             // twiml.message('We have received your request for assistance. We contacted you health worker to coordinate a response.');
             // response.writeHead(200, { 'Content-Type': 'text/xml' });
             // response.end(twiml.toString());
-        } else if (Body.toLowerCase().match(/^(register|start)$/)) {
-            db.checkPhoneNumber(From).then(response => {
-                if (response.length){
-                    console.log('You\'re already registered')
+        } else if (Body.toLowerCase().match(/^.*(register|start).*$/)) {
+            db.checkPhoneNumber(From).then(checkPhoneNumber => {
+                if (checkPhoneNumber.length){
+                    return respond('You\'re already registered')
                 } else {
                     db.addPhone(From).then(addedNumbers =>{
+                        // setTimeout(() => respond('Question 1: What is your 3 word address?'), 10000)
+                        return respond(`Your number has been added. Please complete our survey. ${surveyQs[0].name}`)
+                        // return respond('Question 1: What is your name?')
                         res.status(200).send(addedNumbers)
-                        console.log('Your number has been added.')
-                        setTimeout(() => console.log('Question 1: What is your name?'), 3000)
                     })
                 }
                 // console.log(111111, response)
@@ -33,14 +42,14 @@ module.exports = {
             })
         } else {
             db.checkPhoneNumber(From).then(surveyNumberCheck => {
-                console.log(2343423, surveyNumberCheck[0])
+                // console.log(2343423, surveyNumberCheck[0])
                 if(surveyNumberCheck.length === 0) {
-                    console.log('Did you mean to register for HealthGrid Field Service? Response with Register.')
+                    return respond('Did you mean to register for HealthGrid Field Service? Respond with Register.')
                 } else if(surveyNumberCheck[0].completed === true){
-                    console.log('Survey has already been completed.')
+                    return respond('Survey has already been completed.')
                 } else {
                         if (Object.values(surveyNumberCheck[0]).indexOf(null) === -1){
-                            console.log('Survey is complete. Message us if there is an emergency.')
+                            return respond('Survey is complete. Message us if there is an emergency.')
                         } else {
 
                             // ********** Determine what cell the message body should fill ********** //
@@ -49,7 +58,7 @@ module.exports = {
                             let objectArr = Object.values(surveyNumberCheck[0])
                             // console.log(objectArr)
                             let firstNullIndex = objectArr.indexOf(null)
-                            console.log(firstNullIndex)
+                            // console.log(firstNullIndex)
                         
                             //** Question to fill holds the key of the object at the first null value
                             //** We will add this value to the database
@@ -58,16 +67,10 @@ module.exports = {
                             //** This should be a DB Query for adding values to the database
                             let newObj = {}
                             newObj[questionToFill] = Body;
-
-                            console.log('newObj', newObj)
-                            // console.log("phone", db.mama_survey.columns)
-                            console.log('From', From)
                             
                             db.mama_survey.update({phone: From}, newObj).then( function(error, updatedDatabase) {
                                 res.status(200).send(updatedDatabase)
                             })
-                        
-
 
                             // ********** Determine what question to send ********** //
                     
@@ -82,14 +85,10 @@ module.exports = {
                                 // console.log('This is the reference to the survey question we want to send to the user:', questionToSend)
                                 surveyQs.forEach( element => {
                                     if(questionToSend in element){
-                                        // console.log("This is the survey question we want to ask the user:", Object.values(element)[0])
+                                        return respond(Object.values(element)[0])
                                     }
                                 })
                                 }
-                            // console.log(questionToFill)
-
-
-
                         }
                     }
                 })
